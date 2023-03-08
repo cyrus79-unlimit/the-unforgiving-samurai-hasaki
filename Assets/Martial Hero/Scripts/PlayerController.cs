24357f07ba98cb7571de64bda2cf46f397b6da02
cyrus79-unlimit <giahuy12377@gmail.com>
2023-03-08 18:56:25 +0700

    update character controller script

--- Assets/Martial Hero/Scripts/PlayerController.cs
+++ Assets/Martial Hero/Scripts/PlayerController.cs
@@ -2,55 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-	private Rigidbody2D rb;
-	private Animator animator;
-
 	public float speed = 5f;
 	public float jumpforce = 5f;
+	private Rigidbody2D rb;
 	private bool isGround = false;
 	private Vector3 rotation;
 
 	private bool isRunning = false;
+	private bool isJumping = false;
+
+	private Animator m_animator;
+	private Rigidbody2D m_body2d;
 
 	public Transform attackPoint;
 	public float attackRange = 0.5f;
 	public LayerMask enemyLayers;
 
-
-
-	private void Start()
+	// Use this for initialization
+	void Start()
 	{
-		animator = GetComponent<Animator>();
-		rb = GetComponent<Rigidbody2D>();
+		m_animator = GetComponent<Animator>();
+		m_body2d = GetComponent<Rigidbody2D>();
 		rotation = transform.eulerAngles;
 	}
 
-	private void Update()
+	// Update is called once per frame
+	void Update()
 	{
-		/*if (!m_grounded)
+		// -- Handle input and movement --
+		/*float inputX = Input.GetAxis("Horizontal");
+		if(inputX != 0)
 		{
-			m_grounded = true;
-			
-		}
-
-		//Check if character just started falling
-		if (m_grounded)
-		{
-			m_grounded = false;
-		}
-		// Check for movement input
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		if (moveHorizontal != 0)
-		{
-			// Move the character horizontally
-			rb.velocity = new Vector2(moveHorizontal * moveSpeed, rb.velocity.y);
+			// Move
+			m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
 			isRunning = true;
 
-			// Flip the character sprite if moving left
 			Vector3 characterScale = transform.localScale;
-			if (moveHorizontal < 0)
+			if (inputX < 0)
 			{
 				characterScale.x = -1;
 			}
@@ -63,17 +53,21 @@ public class CharacterController : MonoBehaviour
 		else
 		{
 			isRunning = false;
-		}
-
-		// Check for jump input
-		if (Input.GetKeyDown("space") && m_grounded)
+		}*/
+		// Swap direction of sprite depending on walk direction
+		/*if (inputX < 0)
 		{
-			animator.SetTrigger("Jump");
-			m_grounded = false;
-			animator.SetBool("Grounded", m_grounded);
-			rb.velocity = new Vector2(rb.velocity.x, m_jumpForce);
-			// Jump the character
-			*//*rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);*//*
+			transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+			isRunning = true;
+		}	
+		else if (inputX > 0)
+		{
+			transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+			isRunning = true;
+		}
+		else
+		{
+			isRunning = false;
 		}*/
 
 		float xMove = Input.GetAxis("Horizontal");
@@ -82,41 +76,63 @@ public class CharacterController : MonoBehaviour
 			isRunning = true;
 			transform.localScale = new Vector3(-1, 1, 1);
 			transform.Translate(Vector2.left * speed * -xMove * Time.deltaTime);
-
+			
 		}
 		else if (xMove > 0)
 		{
 			isRunning = true;
 			transform.localScale = new Vector3(1, 1, 1);
-			transform.Translate(Vector2.right * speed * xMove * Time.deltaTime);
+			transform.Translate(Vector2.right * speed * xMove * Time.deltaTime);		
 		}
 		else
 		{
 			isRunning = false;
 		}
-
+		if (isGround == false)
+		{
+			print("is jump");
+		}
+		else
+		{
+			print("NOT jump");
+		}
 		if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
 		{
 			rb.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
-			animator.SetTrigger("Jump");
+			/*isJumping = true;*/
 			isGround = false;
 		}
-		
+		/*else
+		{
+			isJumping = false;
+		}*/
 
+
+		//Set AirSpeed in animator
+		/*m_animator.SetFloat("AirSpeed", m_body2d.velocity.y);
+
+		// -- Handle Animations --
+		//Attack
 		if (Input.GetKeyDown(KeyCode.Z))
 		{
-			animator.SetTrigger("isAttacking1");
+			m_animator.SetTrigger("isAttacking1");
 			Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
 
-			foreach(Collider2D enemy in hitEnemies)
+			foreach (Collider2D enemy in hitEnemies)
 			{
 				enemy.GetComponent<EnemyController>().TakeDamage(25);
 			}
 		}
-		if (Input.GetKeyDown(KeyCode.X))
+
+		//Jump
+		if (Input.GetKeyDown("space") *//*&& m_grounded*//*)
 		{
-			animator.SetTrigger("isAttacking2");
-		}
+			m_animator.SetTrigger("jump");
+			m_grounded = false;
+			m_animator.SetBool("Grounded", m_grounded);
+			m_body2d.velocity = new Vector2(m_body2d.velocity.x, m_jumpForce);
+			m_groundSensor.Disable(0.2f);
+		}*/
 	}
 
 	private void OnDrawGizmosSelected()
@@ -138,6 +154,7 @@ public class CharacterController : MonoBehaviour
 	private void FixedUpdate()
 	{
 		// Update the animator based on the character's movement state
-		animator.SetBool("isRunning", isRunning);
+		m_animator.SetBool("isRunning", isRunning);
+		/*m_animator.SetBool("isJumping", isJumping)*/;
 	}
 }
