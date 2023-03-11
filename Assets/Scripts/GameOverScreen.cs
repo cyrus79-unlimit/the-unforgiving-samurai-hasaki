6c8a782170173c8678ee690bd527b155a6d709d7
cyrus79-unlimit <giahuy12377@gmail.com>
2023-03-11 21:46:33 +0700

    update game over ui

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -5,14 +5,22 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameOverScreen : MonoBehaviour
-{   
+{
+    public float freezeDelay = 1.0f;
     public void SetUp()
     {
+        Invoke("FreezeScene", freezeDelay);
         gameObject.SetActive(true);
     }
+
+    private void FreezeScene()
+    {
+        Time.timeScale = 0f; // freeze the scene
+    }
+
     public void RestartButton()
     {
-        Debug.Log("Working");
+        Time.timeScale = 1.0f;
         Scene currentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(currentScene.name);
     }
